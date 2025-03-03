f205f1da79cda1eadd07b94a16d6f73e6767edb4
llins <llinsomoudu@gmail.com>
2025-03-03 12:11:21 +0100

    Add Home component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/Header';
+export default function HeaderComponent() {
+    return (
+      <header className="bg-blue-600 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold">PetMedTrack</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              <li><a href="#" className="hover:text-blue-200">Home</a></li>
+              <li><a href="#" className="hover:text-blue-200">About</a></li>
+              <li><a href="#" className="hover:text-blue-200">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
